954a4c8c5d8478cecce19239f25a99b94ac85478
Stephane48 <skamdem-kamguia@myseneca.ca>
2024-04-17 19:42:23 -0400

    Day 3

---
+++ Day 1 - work with variables in python.py
@@ -0,0 +1,16 @@
+Print: Prints a string into the console. print("Hello World")
+
+This is an example of how to use quotes in Python:
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+Example of string concatenation: print("Hello" + " " + "Angela")
+
+PEMDAS
+Parentheses ()
+Exponents **
+Multiplication *
+Division /
+Addition +
+Subtraction -
