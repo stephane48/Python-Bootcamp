1fe3e03c6afabdad5a4cacef4cc5e695736dab72
Stephane48 <skamdem-kamguia@myseneca.ca>
2024-04-28 09:33:02 -0400

    Day 19 End

---
+++ Day 19 - Event listener, state.py
@@ -0,0 +1,5 @@
+when the user types a scpecific key on the keyboard 
+
+Even lsiteners is a way to listen what the user does
+
+A higher order function is a function that works with other function  
