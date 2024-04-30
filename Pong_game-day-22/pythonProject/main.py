5c793f0d34315539b8d9be2666b8af158b0911c6
Stephane48 <skamdem-kamguia@myseneca.ca>
2024-04-29 21:18:58 -0400

    Day 22 - The Pong Game

---
+++ Pong_game-day-22/pythonProject/main.py
@@ -0,0 +1,42 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+
+
+
+
+
+
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+
+screen.exitonclick()
