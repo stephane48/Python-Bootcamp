e99dbdbc755424730ea81b86e87e3ed229f97a67
Stephane48 <skamdem-kamguia@myseneca.ca>
2024-05-06 21:05:11 -0400

    Day 32 - Email SMTP, Datetime & Birthday Wisher Project

---
+++ Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,64 @@
+import random
+import smtplib
+import datetime as dt
+
+MY_EMAIL = "yumpom306@gmail.com"
+MY_PASSWORD = "zcbnwfljubgqnalt"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import smtplib
+#
+# my_email = "yumpom306@gmail.com"
+# password = "zcbnwfljubgqnalt"
+#
+#
+# with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="sessegnon.stephane48@gmail.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
+
+# import datetime as dt
+#
+# now = dt.datetime.now()  # This get the current date and time
+# year = now.year
+# month = now.month
+# day_of_the_week = now.weekday()
+# print(day_of_the_week)
+#
+# date_of_birth = dt.datetime(year=2000, month=6, day=9, hour=2)
+# print(date_of_birth)
---
+++ Day 32 - Email SMTP & Datetime Module.py
@@ -0,0 +1,3 @@
+Emial SMTP is a python module that helps us send email using python ConnectionAbortedError
+Dattetime helps to format date properly
+SMTP: Simple Mail Transfert Protocal
