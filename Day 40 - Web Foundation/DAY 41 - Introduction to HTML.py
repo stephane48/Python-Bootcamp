950022894fc67e892531b2559dea1d24f643bf97
Kamdem Kamguia <stephane.kamdemkamguia@cgi.com>
2024-05-11 17:39:28 -0400

    Day 43 - Introduction to CSS

---
+++ Day 40 - Web Foundation/DAY 41 - Introduction to HTML.py
@@ -0,0 +1,7 @@
+HTML (HyperText Markup Language) is responsible of the structure and the content of the website
+CSS (Casscading Style Sheets) files are responsible to styling your website they way you want
+js file 
+
+class selector:
+    is what we can add as an attribute to any html element.
+    it groups all elements in the same format
