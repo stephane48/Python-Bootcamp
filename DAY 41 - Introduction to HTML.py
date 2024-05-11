d5bd54a34793f0c3de3005f7894adc51d99247ab
Stephane48 <skamdem-kamguia@myseneca.ca>
2024-05-11 11:41:11 -0400

    Day 40 - Web Foundation

---
+++ DAY 41 - Introduction to HTML.py
@@ -0,0 +1,3 @@
+HTML (HyperText Markup Language) is responsible of the structure and the content of the website
+CSS (Casscading Stylish Style) files are responsible to styling your website they way you want
+js file 
