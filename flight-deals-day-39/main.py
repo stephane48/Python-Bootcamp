d7855b3e4900e3a80e11b44f8e3b0dd9552ea42f
Stephane48 <skamdem-kamguia@myseneca.ca>
2024-05-11 10:27:06 -0400

    Day 39 - flight-deals-day-39-and-40

---
+++ flight-deals-day-39/main.py
@@ -0,0 +1,7 @@
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager
+# classes to achieve the program requirements.
+
+api_key = ""
+api_secret = ""
+
+https://replit.com/@stephanekamdem/flight-deal-finder-project-day-30-and-40
